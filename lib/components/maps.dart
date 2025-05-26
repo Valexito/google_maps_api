ca3f544362db794e634d93089ad11ba2d3e2c7d3
Valexito <david.argueta@guatestar.org>
2025-05-25 21:28:41 -0600

    Map added and display default location

---
+++ lib/components/maps.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  static const CameraPosition _initialCameraPosition = CameraPosition(
+    target: LatLng(37.773972, -122.431297), // Latitud y longitud corregidas
+    zoom: 11.5,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        initialCameraPosition: _initialCameraPosition,
+          onMapCreated: (controller) {
+    print('Mapa creado!');
+  },
+      ),
+    );
+  }
+}
