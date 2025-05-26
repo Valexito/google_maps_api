679100fa80ac3c7f0731cc5d2ed4d1457477b2ff
Valexito <david.argueta@guatestar.org>
2025-05-25 23:47:32 -0600

    Google Maps implemented successfully

--- lib/components/maps.dart
+++ lib/components/maps.dart
@@ -10,21 +10,119 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   static const CameraPosition _initialCameraPosition = CameraPosition(
-    target: LatLng(37.773972, -122.431297), // Latitud y longitud corregidas
-    zoom: 11.5,
+    target: LatLng(14.834999, -91.518669),// Latitud y longitud corregidas
+    zoom: 13,
   );
+
+  late GoogleMapController _googleMapController;
+  Marker? _origin;
+  Marker? _destination;
+
+  @override void dispose (){
+    _googleMapController.dispose();
+    super.dispose();
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text ('Google Maps'),
+        actions: [
+          TextButton(
+            onPressed: _origin == null 
+            ? null       
+            : ()=>_googleMapController.animateCamera(
+              CameraUpdate.newCameraPosition(
+                CameraPosition(
+                  target: _origin!.position,
+                  zoom: 15,
+                  tilt: 50.0,
+                  ),
+              ),
+            ), 
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.blue,
+              textStyle: const TextStyle(fontWeight: FontWeight.w600),
+            ),
+            child: const Text('ORIGIN'),
+            ),
+          
+           TextButton(
+            onPressed: _destination == null 
+            ? null       
+            : ()=>_googleMapController.animateCamera(
+              CameraUpdate.newCameraPosition(
+                CameraPosition(
+                  target: _origin!.position,
+                  zoom: 15,
+                  tilt: 50.0,
+                  ),
+              ),
+            ), 
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.red,
+              textStyle: const TextStyle(fontWeight: FontWeight.w600),
+            ),
+            child: const Text('DEST'),
+            ),
+        ],
+      ),
       body: GoogleMap(
         myLocationButtonEnabled: false,
         zoomControlsEnabled: false,
         initialCameraPosition: _initialCameraPosition,
-          onMapCreated: (controller) {
-    print('Mapa creado!');
-  },
+        onMapCreated: (controller) {
+          _googleMapController = controller;
+        },
+        markers: {
+          if(_origin != null) _origin!,
+          if(_destination != null) _destination!,
+        },
+        onLongPress: _addMarker,
       ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        foregroundColor: Colors.black,
+        onPressed: () => _googleMapController.animateCamera(
+          CameraUpdate.newCameraPosition(_initialCameraPosition),
+        ),
+          child: const Icon(Icons.center_focus_strong),
+        ),
     );
+  }
+
+  void _addMarker(LatLng pos){
+    if (_origin == null || (_origin != null && _destination != null)){
+      //origin is not set or Origin/Destination are both set
+      //set origin
+      setState(() {
+        _origin = Marker(
+          markerId: const MarkerId('origin'),
+          infoWindow: const InfoWindow(title: 'Origin'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          position: pos,
+          );
+
+          //reset destination
+          _destination: null;
+      });
+    }else{
+      //origin is already set
+      //Set destination
+      setState(() {
+        _destination = Marker(
+          markerId: const MarkerId('destination'),
+          infoWindow: const InfoWindow(title: 'Destination'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+          position: pos,
+        );
+      });
+    }
+
   }
 }
